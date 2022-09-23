d13728d58d43b8f8eb2add68a92a5a1fe3534f64
jorgesanz1798 <jorgesanz1798@gmail.com>
2022-09-23 14:33:08 +0200

    terminado

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'curso-angular'`, () => {
+  it(`should have as title 'authApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('curso-angular');
+    expect(app.title).toEqual('authApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('curso-angular app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('authApp app is running!');
   });
 });
